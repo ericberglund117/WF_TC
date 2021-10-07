521f5347755a28fcf149bded1ea44355b3c5dd98
Eric Berglund <ericberglund117@gmail.com>
2021-10-07 13:10:09 -0600

    Test page redirection upon click

--- Tests/test.js
+++ Tests/test.js
@@ -1,10 +1,18 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 
 fixture `Fixture1`
     .page('https://www.whatshouldireadnext.com/');
+    const sciFiButton = Selector('a').withAttribute('href', "/subject/science+fiction");
 
 test('User button click', async t => {
-        const sciFiButton = Selector('a').withAttribute('href', "/subject/science+fiction")
         await t 
-            .expect(sciFiButton.with({visibilityCheck: true}).exists).ok();
+            .expect(sciFiButton.with({visibilityCheck: true}).exists).ok()
+            .click(sciFiButton)
     });
+
+test('Page redirection after button click', async t => {
+    let bookLocation = ClientFunction(() => document.location.href)
+    await t
+        .click(sciFiButton)
+        .expect(bookLocation()).contains('https://www.whatshouldireadnext.com/subject/science+fiction')
+});
